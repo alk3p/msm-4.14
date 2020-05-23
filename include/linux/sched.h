22d87b139bae20a95150f3da603a7ef935d71928
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-05-23 22:30:19 +0800

    sched/tune: Introduce a function to disable schedtune boost/bias on demand
    Kang getSchedtune() from DSB by @ joshuous.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -28,6 +28,17 @@
 #include <linux/mm_types_task.h>
 #include <linux/task_io_accounting.h>
 
+extern bool disable_boost;
+
+#ifdef CONFIG_SCHED_TUNE
+int disable_schedtune_boost(char *st_name, bool disable);
+#else
+static inline int disable_schedtune_boost(char *st_name, bool disable)
+{
+	return 0;
+}
+#endif
+
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
 struct backing_dev_info;
