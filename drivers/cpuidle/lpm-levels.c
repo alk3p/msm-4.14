825fd4e297ee280360ea79dd5ba495c68377e36a
alk3pInjection <webmaster@raspii.tech>
2020-08-15 11:29:06 +0800

    cpuidle: lpm_levels: Reduce MAX_S2IDLE_CPU_ATTEMPTS to 24
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -57,7 +57,7 @@
 #define PSCI_AFFINITY_LEVEL(lvl) ((lvl & 0x3) << 24)
 #define BIAS_HYST (bias_hyst * NSEC_PER_MSEC)
 
-#define MAX_S2IDLE_CPU_ATTEMPTS  32   /* divide by # cpus for max suspends */
+#define MAX_S2IDLE_CPU_ATTEMPTS  24   /* divide by # cpus for max suspends */
 
 enum {
 	MSM_LPM_LVL_DBG_SUSPEND_LIMITS = BIT(0),
