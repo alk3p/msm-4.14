c4f4bd44963e4dc8ba14b215409b593b9a1170e9
alk3pInjection <webmaster@raspii.tech>
2020-10-06 22:19:56 +0800

    helper: Adapt for overwriting learned capacity
    * Subsequent to ("power_supply: Allow users to overwrite learned capacity")
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- drivers/misc/icekramel_helper.c
+++ drivers/misc/icekramel_helper.c
@@ -4,3 +4,8 @@ int is_fod;
 
 MODULE_PARM_DESC(is_fod, "Activate FOD fix");
 module_param_named(is_fod, is_fod, int, 0644);
+
+unsigned int remove_op_capacity;
+
+MODULE_PARM_DESC(is_custombatt, "Remove battery capacity limit");
+module_param_named(is_custombatt, remove_op_capacity, uint, 0444);
