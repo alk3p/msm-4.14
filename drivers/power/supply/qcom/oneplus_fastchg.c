cf12979f890c96e2e589a578b18678dc0aacbfa3
atndko <z1281552865@gmail.com>
2020-10-06 22:19:36 +0800

    power_supply: Allow users to overwrite learned capacity
    * This squashes the following commits:
    
      atndko/Neptune_kernel_sm8150_oneplus@69008e3
      atndko/Neptune_kernel_sm8150_oneplus@2223793
      atndko/Neptune_kernel_sm8150_oneplus@3f4fe69
      atndko/Neptune_kernel_sm8150_oneplus@0d83681
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit aa914e19855c85a536ce2130f122d66ea05448a3)

--- drivers/power/supply/qcom/oneplus_fastchg.c
+++ drivers/power/supply/qcom/oneplus_fastchg.c
@@ -29,6 +29,10 @@
 
 #define SHOW_FW_VERSION_DELAY_MS 18000
 
+#ifdef CONFIG_REMOVE_OP_CAPACITY
+extern unsigned int remove_op_capacity;
+#endif /* CONFIG_REMOVE_OP_CAPACITY */
+
 struct fastchg_device_info {
 	struct i2c_client		*client;
 	struct miscdevice   dash_device;
@@ -1248,8 +1252,16 @@ static long  dash_dev_ioctl(struct file *filp, unsigned int cmd,
 				di->fast_chg_ing = true;
 				volt = onplus_get_battery_mvolts();
 				temp = onplus_get_battery_temperature();
+#ifdef CONFIG_REMOVE_OP_CAPACITY
+				if (!remove_op_capacity) {
+					remain_cap = onplus_get_batt_remaining_capacity();
+				} else {
+					remain_cap = -EINVAL;
+				}
+#else /* CONFIG_REMOVE_OP_CAPACITY */
 				remain_cap =
 				onplus_get_batt_remaining_capacity();
+#endif /* CONFIG_REMOVE_OP_CAPACITY */
 				soc = onplus_get_battery_soc();
 				current_now = onplus_get_average_current();
 				pr_err("volt:%d,temp:%d,remain_cap:%d,soc:%d,current:%d\n",
