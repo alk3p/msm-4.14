7ee9996f449726f7d8b11d71f73e084946884fbb
alk3pInjection <webmaster@raspii.tech>
2020-10-10 10:42:50 +0800

    Revert "security: Kill SELinux"
    This reverts commit f9263efc9da1b5ecec4a9f832573be79d9e2a86a.
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -1026,8 +1026,6 @@ static noinline int avc_denied(struct selinux_state *state,
 			       u8 driver, u8 xperm, unsigned int flags,
 			       struct av_decision *avd)
 {
-	return 0;
-
 	if (flags & AVC_STRICT)
 		return -EACCES;
 
