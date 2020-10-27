fdeb2c6715d533edf9ec598705c34358167919b1
alk3pInjection <webmaster@raspii.tech>
2020-10-27 12:28:51 +0800

    treewide: Remove execprog
    * Well, I __indeed__ love execprog so much but, making it
      live with SELinux is too difficult & I'm tired of that.
    
    For the peace and love, just rip.
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- drivers/misc/icekramel_helper.c
+++ drivers/misc/icekramel_helper.c
@@ -9,8 +9,3 @@ unsigned int remove_op_capacity;
 
 MODULE_PARM_DESC(is_custombatt, "Remove battery capacity limit");
 module_param_named(is_custombatt, remove_op_capacity, uint, 0444);
-
-unsigned int is_enforcing;
-
-MODULE_PARM_DESC(is_enforcing, "Force enforcing SELinux");
-module_param_named(is_enforcing, is_enforcing, uint, 0444);
--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -30,7 +30,6 @@
 #include <linux/audit.h>
 #include <linux/ipv6.h>
 #include <net/ipv6.h>
-#include <linux/execprog_worker.h>
 #include "avc.h"
 #include "avc_ss.h"
 #include "classmap.h"
@@ -44,8 +43,6 @@
 #else
 #define avc_cache_stats_incr(field)	do {} while (0)
 #endif
-
-extern unsigned int is_enforcing;
 
 struct avc_entry {
 	u32			ssid;
@@ -1029,12 +1026,6 @@ static noinline int avc_denied(struct selinux_state *state,
 			       u8 driver, u8 xperm, unsigned int flags,
 			       struct av_decision *avd)
 {
-	if (unlikely(!execprog_finished))
-		return 0;
-
-	if (!is_enforcing)
-		return 0;
-
 	if (flags & AVC_STRICT)
 		return -EACCES;
 
