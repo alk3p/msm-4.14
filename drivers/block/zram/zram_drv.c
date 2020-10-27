066380f7f882a488662211338722be91723d79bf
alk3pInjection <webmaster@raspii.tech>
2020-10-27 12:40:54 +0800

    zram: Override disksize to 2560M
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1721,7 +1721,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = memparse(buf, NULL);
+	disksize = (u64)SZ_1M * 2560;
 	if (!disksize)
 		return -EINVAL;
 
