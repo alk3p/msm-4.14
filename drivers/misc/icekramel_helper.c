110ec69323af835d26d27cc39eea753075d4843e
alk3pInjection <webmaster@raspii.tech>
2020-12-20 16:04:01 +0800

    helper: Fix un-initialized variables
    * using ak3 to initialize them is not actually reliable
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit 86467f75238877002e62241f5955b388fb65b79c)

--- drivers/misc/icekramel_helper.c
+++ drivers/misc/icekramel_helper.c
@@ -1,11 +1,11 @@
 #include <linux/module.h>
 
-unsigned int is_fod;
+unsigned int is_fod = 0;
 
 MODULE_PARM_DESC(is_fod, "Activate FOD fix");
 module_param_named(is_fod, is_fod, uint, 0644);
 
-unsigned int remove_op_capacity;
+unsigned int remove_op_capacity = 0;
 
 MODULE_PARM_DESC(is_custombatt, "Remove battery capacity limit");
 module_param_named(is_custombatt, remove_op_capacity, uint, 0444);
