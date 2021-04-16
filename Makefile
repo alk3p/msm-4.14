alk3p/msm-4.14

commit c72457d94562465f4b00b8904ba0a91a820897ea

Makefile: Prohibit VLA
Signed-off-by: alk3pInjection <webmaster@raspii.tech>

Makefile
@@ -434,6 +434,8 @@ ifeq ($(CONFIG_EARLY_INIT),true)
 KBUILD_CFLAGS    += -DCONFIG_EARLY_SERVICES
 endif
 
+KBUILD_CFLAGS    += -Werror=vla
+
 KBUILD_CPPFLAGS := -D__KERNEL__
 KBUILD_AFLAGS_KERNEL :=
 KBUILD_CFLAGS_KERNEL :=

commit ad0b3bfbd06023ed8669c3495eb786d8e260dfb7

Makefile: Guard -Wvla
Fixes: c72457d

Makefile
@@ -434,7 +434,9 @@ ifeq ($(CONFIG_EARLY_INIT),true)
 KBUILD_CFLAGS    += -DCONFIG_EARLY_SERVICES
 endif
 
+ifeq ($(cc-name),gcc)
 KBUILD_CFLAGS    += -Werror=vla
+endif
 
 KBUILD_CPPFLAGS := -D__KERNEL__
 KBUILD_AFLAGS_KERNEL :=